9d4804477f221474feeabc025e5081d07a899485
SergiySobolev <sergiy.sobolev@gmail.com>
2016-07-27 23:50:23 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: `
+        <html>
+            <body>
+                <div>My App with webpack</div> 
+            </body>
+        </html>
+    `
+})
+
+export class AppComponent {
+    
+}
